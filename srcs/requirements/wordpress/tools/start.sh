2850d4ea76573e66c4b47a502a03850739ab4934
root <root@wfermey.wfermey.42.fr>
2022-10-27 13:10:22 +0200

    add all

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -3,7 +3,7 @@
 if [ ! -f "/var/www/html/wordpress/index.php" ]; then
 	wp core download --allow-root
 	wp config create --dbname=$DB_NAME --dbuser=$DB_USER_NAME --dbpass=$DB_USER_PWD --dbhost=$DB_SERVER --dbcharset="utf8"  --allow-root
-	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email   --allow-root
+	wp core install --url=wfermey.42.fr --title=$WP_TITLE --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email   --allow-root
 	wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD   --allow-root
 	wp theme install inspiro --activate --allow-root
 
